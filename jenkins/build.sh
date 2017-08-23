3c550c74550bd9903c637c0f1fbd105ede961ffa
Your Name <you@example.com>
2017-08-23 11:39:39 +0800

    update jenkins 2.7

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #usage: ansible-galaxy install -r requirements.yml --force
 
+#Extensible Choice Parameter plugin
+#apt-add-repository ppa:ansible/ansible
+#apt-get install -y ansible
+
 /usr/bin/docker build -t="registry.dkhs.com/jenkins-ansible" .
 
